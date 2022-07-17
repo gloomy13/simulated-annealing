fae863d2c9c8c09708019a7fe1d71044080085ec
gloomy13 <74317881+gloomy13@users.noreply.github.com>
2022-07-17 14:46:25 +0200

    init

---
+++ Procesor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Procesor : ICloneable
+{
+    List<Zadanie> Zadania = new List<Zadanie>();
+    public uint indeks;
+    public uint czas = 0;
+
+    public Procesor(uint indeks)
+    {
+        this.indeks = indeks;
+    }
+    public void DodajZadanie(Zadanie zadanie, uint czasWykonania)
+    {
+        zadanie.czasWykonania = czasWykonania;
+        Zadania.Add(zadanie);
+    }
+    public Zadanie WykonajZadanie(Zadanie zadanie)
+    {
+        Zadania[(int)zadanie.indeks].czasRozpoczecia = czas;
+        czas += Zadania[(int)zadanie.indeks].czasWykonania;
+        Zadania[(int)zadanie.indeks].czasZakonczenia = czas;
+        return Zadania[(int)zadanie.indeks];
+        //zadanie.wykonane = true; to trzeba bedzie zrobic w kolejkowaniu
+    }
+    public void PrzesuniecieZadanWCzasie(uint przesuniecie, uint odKiedy) // w przypadku czakania na zakonczenie procesu
+    {
+        czas += przesuniecie;
+        foreach (var zadanie in Zadania)
+        {
+            if (zadanie.czasRozpoczecia >= odKiedy)
+            {
+                zadanie.czasRozpoczecia += przesuniecie;
+                zadanie.czasZakonczenia += przesuniecie;
+            }
+        }
+    }
+    public object Clone()
+    {
+        Procesor klon = new Procesor(this.indeks);
+        klon.Zadania = this.Zadania;
+
+        return klon;
+    }
+}
---
+++ Rozwiazanie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Rozwiazanie
+{
+    public List<Tuple<Procesor, Zadanie>> paryPZ = new List<Tuple<Procesor, Zadanie>>();
+    public uint czas = 0;
+    public Rozwiazanie() { }
+    public void DodajKrok(Tuple<Procesor, Zadanie> para) //dodaje krok w kolejnosci do rozwiazania (para procesor/zadanie)
+    {
+        var para2 = new Tuple<Procesor, Zadanie>(para.Item1, para.Item1.WykonajZadanie(para.Item2));
+        paryPZ.Add(para2);
+    }
+    public uint ZliczCzas()
+    {
+        foreach (var para in paryPZ)
+            if (para.Item2.zalezneOd.Count != 0)
+                foreach (var para2 in paryPZ)
+                    foreach (var zaleznosc in para.Item2.zalezneOd)
+                        if (para2.Item2.indeks == zaleznosc.indeks)
+                            if (para2.Item2.czasZakonczenia > para.Item2.czasRozpoczecia)
+                            {
+                                uint przesuniecie = para2.Item2.czasZakonczenia - para.Item2.czasRozpoczecia; // czekanie zwieksza czas pracy procesora
+                                para.Item1.PrzesuniecieZadanWCzasie(przesuniecie, para.Item2.czasRozpoczecia);
+                            }
+        czas = paryPZ.Select(x => x.Item1).Distinct().Max(x => x.czas);
+        return czas; // czas pracy porcesora który kończy ostatni
+    }
+    public string DrukRozwiązania()
+    {
+        string kolejność = "";
+        foreach (var el in paryPZ)
+        {
+            kolejność += "[p:" + el.Item1.indeks + " - z:" + el.Item2.indeks + "] ";
+        }
+        return kolejność;
+    }
+}
